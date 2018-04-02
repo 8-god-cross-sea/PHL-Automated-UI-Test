f93407346c2cda5e29461686dfc22d1b5bb4a815
ddwing <d_e_ddwing@hotmail.com>
2018-04-02 12:28:33 +0800

    Checkpoint

--- Tests/SmokeTest_Department.py
+++ Tests/SmokeTest_Department.py
@@ -9,3 +9,12 @@ class SmokeTest_Department(unittest.TestCase):
     def tearDown(self):
         self.driver.close()
 
+    def test_Dpetartment_Add(self):
+        DeptMenuItem=self.driver.find_element_by_xpath("/html/body[@class='skin-blue sidebar-mini']/div[@class='wrapper']/aside[@class='main-sidebar']/section[@class='sidebar']/ul[@class='sidebar-menu tree']/li[@class='treeview menu-open']/ul[@class='treeview-menu']/li[1]/a")
+        DeptMenuItem.click()
+        AddButton=self.driver.find_element_by_xpath("/html/body[@class='skin-blue sidebar-mini']/div[@class='wrapper']/div[@class='content-wrapper']/section[@class='content container-fluid']/div[@class='row']/div[@class='col-xs-12']/div[@class='box']/div[@class='box-body']/table[@id='example2']/thead[@id='thead']/tr/th[4]/a")
+        AddButton.click()
+        NameInput=self.driver.find_element_by_xpath("/html/body[@class='skin-blue sidebar-mini']/div[@class='wrapper']/div[@class='content-wrapper']/section[@class='content container-fluid']/div[@class='row']/div[@class='col-xs-12']/div[@class='box']/div[@class='box-body']/table[@id='example2']/tfoot[@id='tfoot']/tr/td[2]/input")
+        NameInput.send_keys("Test")
+        DescriptionInput=self.driver.find_element_by_xpath("/html/body[@class='skin-blue sidebar-mini']/div[@class='wrapper']/div[@class='content-wrapper']/section[@class='content container-fluid']/div[@class='row']/div[@class='col-xs-12']/div[@class='box']/div[@class='box-body']/table[@id='example2']/tfoot[@id='tfoot']/tr/td[3]/input")
+        DescriptionInput.send("Descrption")
